86cff87d50643fcd65ae476b071055bd8a77fcc1
Igboama Winifred <winiigboama@gmail.com>
2024-09-11 10:31:11 +0100

    add django url routing to templates

--- freelancer/pages/urls.py
+++ freelancer/pages/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "pages"
+
 urlpatterns = [
-    path("services/", views.services, name="services"),
-    path("testimonials/", views.testimonials, name="testimonials"),
+    path("services-offered/", views.services, name="services"),
+    path("client-testimonials/", views.testimonials, name="testimonials"),
     path("case-studies/", views.case_studies, name="case_studies"),
     path("blog/", views.blog, name="blog"),
     path("pricing/", views.pricing, name="pricing"),
